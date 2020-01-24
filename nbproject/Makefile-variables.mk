57f46f15cdc1ebf84c898457c683ba6d9ab5f42f
David Castells-Rufas <david.castells@uab.cat>
2020-01-24 18:52:17 +0100

    netbeans necessary files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,51 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Cygwin-Debug configuration
+CND_PLATFORM_Cygwin-Debug=GNU-Linux
+CND_ARTIFACT_DIR_Cygwin-Debug=dist/Cygwin-Debug/GNU-Linux
+CND_ARTIFACT_NAME_Cygwin-Debug=pedestriandetectionbenchmark
+CND_ARTIFACT_PATH_Cygwin-Debug=dist/Cygwin-Debug/GNU-Linux/pedestriandetectionbenchmark
+CND_PACKAGE_DIR_Cygwin-Debug=dist/Cygwin-Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Cygwin-Debug=pedestriandetectionbenchmark.tar
+CND_PACKAGE_PATH_Cygwin-Debug=dist/Cygwin-Debug/GNU-Linux/package/pedestriandetectionbenchmark.tar
+# Cygwin-Release configuration
+CND_PLATFORM_Cygwin-Release=GNU-Linux
+CND_ARTIFACT_DIR_Cygwin-Release=dist/Cygwin-Release/GNU-Linux
+CND_ARTIFACT_NAME_Cygwin-Release=pedestriandetectionbenchmark
+CND_ARTIFACT_PATH_Cygwin-Release=dist/Cygwin-Release/GNU-Linux/pedestriandetectionbenchmark
+CND_PACKAGE_DIR_Cygwin-Release=dist/Cygwin-Release/GNU-Linux/package
+CND_PACKAGE_NAME_Cygwin-Release=pedestriandetectionbenchmark.tar
+CND_PACKAGE_PATH_Cygwin-Release=dist/Cygwin-Release/GNU-Linux/package/pedestriandetectionbenchmark.tar
+# Ubuntu-Debug configuration
+CND_PLATFORM_Ubuntu-Debug=GNU-Linux
+CND_ARTIFACT_DIR_Ubuntu-Debug=dist/Ubuntu-Debug/GNU-Linux
+CND_ARTIFACT_NAME_Ubuntu-Debug=pedestriandetectionbenchmark
+CND_ARTIFACT_PATH_Ubuntu-Debug=dist/Ubuntu-Debug/GNU-Linux/pedestriandetectionbenchmark
+CND_PACKAGE_DIR_Ubuntu-Debug=dist/Ubuntu-Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Ubuntu-Debug=pedestriandetectionbenchmark.tar
+CND_PACKAGE_PATH_Ubuntu-Debug=dist/Ubuntu-Debug/GNU-Linux/package/pedestriandetectionbenchmark.tar
+# Ubuntu-Release configuration
+CND_PLATFORM_Ubuntu-Release=GNU-Linux
+CND_ARTIFACT_DIR_Ubuntu-Release=dist/Ubuntu-Release/GNU-Linux
+CND_ARTIFACT_NAME_Ubuntu-Release=pedestriandetectionbenchmark
+CND_ARTIFACT_PATH_Ubuntu-Release=dist/Ubuntu-Release/GNU-Linux/pedestriandetectionbenchmark
+CND_PACKAGE_DIR_Ubuntu-Release=dist/Ubuntu-Release/GNU-Linux/package
+CND_PACKAGE_NAME_Ubuntu-Release=pedestriandetectionbenchmark.tar
+CND_PACKAGE_PATH_Ubuntu-Release=dist/Ubuntu-Release/GNU-Linux/package/pedestriandetectionbenchmark.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
